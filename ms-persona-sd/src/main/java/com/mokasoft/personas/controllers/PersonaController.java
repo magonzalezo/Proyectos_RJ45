169e8109408cfbdf31d30fdc8a8e7b322f1e6bc5
Miguel <miguel.gonzalez.ordonez@gmail.com>
2020-01-27 23:30:20 -0500

    Se agregan mas metodos al servicio y se adjunta el dockerfile y docker-compose

--- ms-persona-sd/src/main/java/com/mokasoft/personas/controllers/PersonaController.java
+++ ms-persona-sd/src/main/java/com/mokasoft/personas/controllers/PersonaController.java
@@ -1,13 +1,14 @@
 package com.mokasoft.personas.controllers;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.mokasoft.personas.entities.PersonaEntity;
 import com.mokasoft.personas.repositories.PersonaRepository;
@@ -26,7 +27,7 @@ public class PersonaController {
     @GetMapping("/personas")
     public List<PersonaEntity> getPersonas(){
     	Iterable<PersonaEntity> result = personaRepository.findAll();
-        List<PersonaEntity> personasList = new ArrayList<PersonaEntity>();
+        List<PersonaEntity> personasList = new ArrayList<>();
         result.forEach(personasList ::add);
         return personasList;
     }
@@ -34,5 +35,25 @@ public class PersonaController {
     @PostMapping("/persona")
 	public void agregarPersona(@RequestBody PersonaEntity nueva) {
     	personaRepository.save(nueva);
-	}    
+	}
+
+	@GetMapping("/persona/{id}") //Consultar Persona
+    public ResponseEntity <PersonaEntity> getPersonaByID(@PathVariable("id") Integer id){
+        PersonaEntity personaEncontrada = personaRepository.findById(id).get();
+        return new ResponseEntity(personaEncontrada, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/persona/{id}")
+    public ResponseEntity <Void> borrarPersona(@PathVariable("id") Integer id){
+        personaRepository.deleteById(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/personas/{genero}/{fecha}")
+    public ResponseEntity <List<PersonaEntity>> getPersonaByGeneroAndFecNac(@PathVariable("genero") String genero, @PathVariable("fecha") @DateTimeFormat(pattern="ddMMyyyy") Date fecha_Nacimiento){
+        Iterable<PersonaEntity> result = personaRepository.findPersonaEntitiesByGeneroAndFechaNacimiento(genero, fecha_Nacimiento);
+        List<PersonaEntity> personasList = new ArrayList<PersonaEntity>();
+        result.forEach(personasList ::add);
+        return new ResponseEntity(result, HttpStatus.OK);
+    }
 }
--- ms-persona-sd/src/main/java/com/mokasoft/personas/repositories/PersonaRepository.java
+++ ms-persona-sd/src/main/java/com/mokasoft/personas/repositories/PersonaRepository.java
@@ -1,11 +1,19 @@
 package com.mokasoft.personas.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.mokasoft.personas.entities.PersonaEntity;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface PersonaRepository extends CrudRepository<PersonaEntity, Integer>{
+
+    @Query("SELECT a FROM PersonaEntity a WHERE a.genero=:genero AND a.fechaNacimiento=:fecha_nacimiento")
+    List<PersonaEntity> findPersonaEntitiesByGeneroAndFechaNacimiento(@Param("genero") String genero, @Param("fecha_nacimiento") Date fecha_nacimiento);
 
 }
