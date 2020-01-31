a11d08644bc6168671e960c1b8601cae99b8b5b8
Miguel <miguel.gonzalez.ordonez@gmail.com>
2020-01-31 00:38:12 -0500

    Se modifican compose con variables y entrypoint

--- ms-persona-sd/entrypoint.sh
+++ ms-persona-sd/entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 echo "Levantando contenedor..."
+echo $POSTGRES_URL
+echo $POSTGRES_PORT
+echo $POSTGRES_USER
+echo $POSTGRES_PASSWORD
+echo "Preparando servicio..."
 java -jar ./target/personas-0.0.1-SNAPSHOT.jar
