493f72c00efc7c10627af548748a31b52fb4ad3e
Miguel <miguel.gonzalez.ordonez@gmail.com>
2020-02-06 23:35:26 -0500

    Se agrega control de excepciones de ejemplo

--- ms-persona-sd/src/main/java/com/mokasoft/personas/controllers/PersonaController.java
+++ ms-persona-sd/src/main/java/com/mokasoft/personas/controllers/PersonaController.java
@@ -1,10 +1,9 @@
 package com.mokasoft.personas.controllers;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -40,9 +39,27 @@ public class PersonaController {
 	}
 
 	@GetMapping("/{id}")
-    public ResponseEntity <PersonaEntity> obtenerPersonaPorID(@PathVariable("id") Integer id){
-        PersonaEntity personaEncontrada = personaRepository.findById(id).get();
-        return new ResponseEntity(personaEncontrada, HttpStatus.OK);
+    public ResponseEntity <?> obtenerPersonaPorID(@PathVariable("id") Integer id){
+        PersonaEntity personaEncontrada = null;
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            personaEncontrada = personaRepository.findById(id).orElse(null);
+
+            if (personaEncontrada == null) {
+                response.put("mensaje","No se pudo encontrar a la persona.");
+                return new ResponseEntity(response, HttpStatus.NOT_FOUND);
+            }
+
+            return new ResponseEntity(personaEncontrada, HttpStatus.OK);
+
+        } catch (DataAccessException dae){
+            response.put("mensaje","Error al consultar en base de datos.");
+            response.put("error", dae.getMessage() + " ### " + dae.getMostSpecificCause().getMessage());
+            return new ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
     }
 
     @DeleteMapping("/{id}")
